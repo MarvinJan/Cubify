b77a8ae5b636b1bc1a2aa1f04678a12fc50c31f0
MarvinJan <marvinjan@mail.ru>
2019-09-09 13:03:06 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Cubify JS plugin 
+
+## Make your DIVs into 3d cubes
+
+This plugin allows to bundle up to 6 (obviously :) ) DIVs into one 3d interactive cube!
+
+## Usage
+
+Link the script in your head tag
+
+```
+<script src='*YOUR__PATH*/cubify.js'></script>
+```
+
+Then choose the DIV you wish to be the front and initiate the plugin passing it as a first parameter: 
+
+```
+new Cubify(*YOUR__FRONT__DIV*, options)
+```
+
+## Options
+
+Second parameter of the plugin, must be an **object**, which can include:
+
+### faces : object
+
+With it you can set another five DIVs as each of five remaining cube faces:
+
+```
+faces: {
+    back: *YOUR__BACK__DIV*
+    left: *YOUR__LEFT__DIV*
+    right: *YOUR__RIGHT__DIV*
+    top: *YOUR__TOP__DIV*
+    bottom: *YOUR__BOTTOM__DIV*
+}
+```
+### interactive : boolean
+
+Option to allow user rotation of the cube via mouse. Default - **false**
+
+### width
+
+The width (and, since it'sm you know - a cube - height and depth) of the cube. Default - largest dimension (meaning height of width) of all passed DIVs; 
---
+++ src/js/cubify.js
@@ -0,0 +1,167 @@
+(function() {
+  class Cubify {
+    constructor(
+      front,
+      options = {
+        interactive: false
+      }
+    ) {
+      this.front = front;
+      this.faces = options.faces || {};
+      this.width = options.width || this.getDefaultWidth();
+      this.parent = this.front.parentNode;
+      this.bgColor = this.getBackgroundColor();
+      this.commonStyle = {
+        width: `${this.width}px`,
+        height: `${this.width}px`
+      };
+      this.sceneStyle = {
+        perspective: `${this.width}px`
+      };
+      this.faceStyle = {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        "background-color": this.bgColor
+      };
+      this.cubeStyle = {
+        transform: `translateZ(-${this.width / 2}px)`,
+        "transform-style": `preserve-3d`
+      };
+      this.interactive = options.interactive;
+      if (options.interactive) {
+        (this.angleX = 0), (this.angleY = 0);
+      }
+      this.renderCube();
+      this.setUpListeners();
+    }
+    getBackgroundColor() {
+      return window
+        .getComputedStyle(this.front, null)
+        .getPropertyValue("background-color");
+    }
+    getDefaultWidth() {
+      let dimensions = [];
+      let pushDimensions = element => {
+        if (!element) {
+          return;
+        } else {
+          dimensions.push(element.offsetWidth, element.offsetHeight);
+        }
+      };
+      pushDimensions(this.front);
+      pushDimensions(this.faces.back);
+      pushDimensions(this.faces.right);
+      pushDimensions(this.faces.top);
+      pushDimensions(this.faces.bottom);
+      pushDimensions(this.faces.left);
+
+      return Math.max(...dimensions);
+    }
+    generateDOM(style, className, elementNode) {
+      const element = elementNode || document.createElement("div");
+      element.classList.add(className);
+      if (typeof style === "string") {
+        this.setStyle(element, style);
+        return element;
+      }
+      const concatStyle = Object.assign({}, this.commonStyle, style);
+      const styleKeys = Object.keys(concatStyle);
+      const styleString = styleKeys
+        .map(key => `${key}: ${concatStyle[key]};`)
+        .join(" ");
+      this.setStyle(element, styleString);
+      return element;
+    }
+    setFaceStyle(face, rotateAxis = "X", degree = 0) {
+      return (face.style.transform = `rotate${rotateAxis}(${degree}deg) translateZ(${this
+        .width / 2}px)`);
+    }
+    setStyle(element, style) {
+      return element.setAttribute("style", style);
+    }
+    composeCube() {
+      const scene = this.generateDOM(this.sceneStyle, "cubify__scene");
+      const cube = this.generateDOM(this.cubeStyle, "cubify__cube");
+
+      const front = this.generateDOM(
+        this.faceStyle,
+        "cubify__face",
+        this.front
+      );
+
+      const back = !this.faces.back
+        ? this.generateDOM(this.faceStyle, "cubify__face")
+        : this.generateDOM(this.faceStyle, "cubify__face", this.faces.back);
+      const top = !this.faces.top
+        ? this.generateDOM(this.faceStyle, "cubify__face")
+        : this.generateDOM(this.faceStyle, "cubify__face", this.faces.top);
+      const bottom = !this.faces.bottom
+        ? this.generateDOM(this.faceStyle, "cubify__face")
+        : this.generateDOM(this.faceStyle, "cubify__face", this.faces.bottom);
+      const right = !this.faces.right
+        ? this.generateDOM(this.faceStyle, "cubify__face")
+        : this.generateDOM(this.faceStyle, "cubify__face", this.faces.right);
+      const left = !this.faces.left
+        ? this.generateDOM(this.faceStyle, "cubify__face")
+        : this.generateDOM(this.faceStyle, "cubify__face", this.faces.left);
+
+      this.setFaceStyle(front, "Y", 0);
+      this.setFaceStyle(back, "Y", 180);
+      this.setFaceStyle(top, "X", 90);
+      this.setFaceStyle(bottom, "X", -90);
+      this.setFaceStyle(right, "Y", 90);
+      this.setFaceStyle(left, "Y", -90);
+
+      cube.append(front, back, top, bottom, left, right);
+      scene.append(cube);
+
+      this.cube = cube;
+
+      return scene;
+    }
+    renderCube() {
+      this.parent.append(this.composeCube());
+    }
+    rotateCube(event, x, y) {
+      const deltaX = this.calculateDelta(x, event.clientX);
+      const deltaY = this.calculateDelta(y, event.clientY);
+      const deltaAngleX = this.angleX - deltaX / 2;
+      const deltaAngleY = this.angleY + deltaY / 2;
+      this.cube.style.transform = `translateZ(-${this.width /
+        2}px) rotateY(${deltaAngleX}deg) rotateX(${deltaAngleY}deg)`;
+      return [deltaAngleX, deltaAngleY];
+    }
+    calculateDelta(initial, current) {
+      return initial - current;
+    }
+    setAngles(deltaAngleX, deltaAngleY) {
+      this.angleX = deltaAngleX;
+      this.angleY = deltaAngleY;
+    }
+    removeListener(element, listener, callback) {
+      element.removeEventListener(listener, callback);
+    }
+    setUpListeners() {
+      this.cube.addEventListener("mousedown", e => {
+        e.preventDefault();
+        const x0 = e.clientX;
+        const y0 = e.clientY;
+        let deltaAngles;
+        const rotate = event => {
+          deltaAngles = this.rotateCube(event, x0, y0);
+          return this.rotateCube(event, x0, y0);
+        };
+        rotate(e, x0, y0);
+        const onDone = () => {
+          this.setAngles(...deltaAngles);
+          document.removeEventListener("mousemove", rotate);
+          document.removeEventListener("mouseup", onDone);
+        };
+        document.addEventListener("mousemove", rotate);
+        document.addEventListener("mouseup", onDone);
+      });
+    }
+  }
+  window.Cubify = Cubify;
+})();
